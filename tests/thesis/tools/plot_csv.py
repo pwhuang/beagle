e557cb8025a0176ba322bbddaf37bdacb6b7e66a
Powei Huang <ph209510@rwth-aachen.de>
2018-02-08 15:42:06 +0100

    changed plot tool.

--- tests/thesis/tools/plot_csv.py
+++ tests/thesis/tools/plot_csv.py
@@ -9,7 +9,7 @@ import pandas as pd
 sys_arg = np.array(sys.argv)
 f = []
 lg = []
-plt.figure()
+plt.figure(figsize=(20,10))
 
 for i, file in enumerate(sys_arg[1:]):
     f.append(pd.read_csv(file))
@@ -19,27 +19,27 @@ for i, file in enumerate(sys_arg[1:]):
 
 
 plt.title('Entropy Production')
-plt.legend(lg)
-plt.show()
+#plt.legend(lg)
+#plt.show()
 
 for i, file in enumerate(sys_arg[1:]):
     f.append(pd.read_csv(file))
-    lg.append(file)
+    #lg.append(file)
     plt.subplot(222)
     plt.plot(f[i]['time'], f[i]['Nusselt'])
 
 
 plt.title('Entropy Production')
-plt.legend(lg)
-plt.show()
+#plt.legend(lg)
+#plt.show()
 
 for i in range(len(sys_arg[1:])):
     plt.subplot(223)
     plt.plot(f[i]['time'], f[i]['max_CFL'])
 
 plt.title('Max CFL Number')
-plt.legend(lg)
-plt.show()
+#plt.legend(lg)
+#plt.show()
 
 plt.figure()
 for i in range(len(sys_arg[1:])):
