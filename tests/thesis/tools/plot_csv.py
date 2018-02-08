131da7cc58c2d1ba21ae60e4e2e9f30adcf1b35d
Powei Huang <ph209510@rwth-aachen.de>
2018-02-08 12:26:23 +0100

    adding Nusselt number compare.

--- tests/thesis/tools/plot_csv.py
+++ tests/thesis/tools/plot_csv.py
@@ -22,8 +22,19 @@ plt.title('Entropy Production')
 plt.legend(lg)
 plt.show()
 
-for i in range(len(sys_arg[1:])):
+for i, file in enumerate(sys_arg[1:]):
+    f.append(pd.read_csv(file))
+    lg.append(file)
     plt.subplot(222)
+    plt.plot(f[i]['time'], f[i]['Nusselt'])
+
+
+plt.title('Entropy Production')
+plt.legend(lg)
+plt.show()
+
+for i in range(len(sys_arg[1:])):
+    plt.subplot(223)
     plt.plot(f[i]['time'], f[i]['max_CFL'])
 
 plt.title('Max CFL Number')
@@ -32,7 +43,7 @@ plt.show()
 
 plt.figure()
 for i in range(len(sys_arg[1:])):
-    plt.subplot(223)
+    plt.subplot(224)
     plt.plot(f[i]['time'], f[i]['max_Peclet'])
 
 plt.title('Max Peclet Number')
