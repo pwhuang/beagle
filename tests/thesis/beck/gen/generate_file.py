497dcb1b6e49daf7187e3b801817b44de44b960c
Powei Huang <ph209510@rwth-aachen.de>
2018-02-19 12:12:07 +0100

    Fixed such that it reads a csv file for nodes.

--- tests/thesis/beck/gen/generate_file.py
+++ tests/thesis/beck/gen/generate_file.py
@@ -1,33 +1,44 @@
 import numpy as np
+import pandas as pd
 import subprocess
 import sys
 
-sys_arg = np.array(sys.argv) #nx, nz
+sys_arg = np.array(sys.argv) #input_csv, start_point, number of cubes to compute
 
-nx_s = sys_arg[1]
-nz_s = sys_arg[2]
+input_data = pd.read_csv(sys_arg[1])
+start_point = int(sys_arg[2])
+cubes_to_compute = int(sys_arg[3])
 
-nx = int(nx_s)
-nz = int(nz_s)
+nx_s = np.array(input_data['h1'], dtype=int)
+nz_s = np.array(input_data['h2'], dtype=int)
+
+nx = nx_s[start_point:start_point+cubes_to_compute]
+nz = nz_s[start_point:start_point+cubes_to_compute]
+
+nodes = np.array([nx, nz]).T
+
 cl = 0.05
 
-xmax = str(nx*cl)
-zmax = str(nz*cl)
+for node in nodes:
+    x = node[0]
+    z = node[1]
 
-string_to_write = 'xmax = ' + xmax + '\n' + 'zmax = ' + zmax + '\n' + 'nx = ' + nx_s + '\n' + 'nz = ' + nz_s + '\n'
+    xmax = str(x*cl)
+    zmax = str(z*cl)
 
+    string_to_write = 'xmax = ' + xmax + '\n' + 'zmax = ' + zmax + '\n' + 'nx = ' + str(x) + '\n' + 'nz = ' + str(z) + '\n'
 
-f = open("voa_beck_base.i", "r")
-contents = f.readlines()
-f.close()
+    f = open("voa_beck_base.i", "r")
+    contents = f.readlines()
+    f.close()
 
-contents.insert(4, string_to_write)
+    contents.insert(4, string_to_write)
 
-file_to_write = "voa_beck_gen" + nx_s + '_' + nz_s + '.i'
-f = open(file_to_write, "w")
-contents = "".join(contents)
-f.write(contents)
-f.close()
+    file_to_write = "voa_beck_gen" + str(x) + '_' + str(z) + '.i'
+    f = open(file_to_write, "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
 
-cmd = "mpirun -n 48 $BEAGLE_DIR/beagle-opt -i " + file_to_write
-returned_value = subprocess.call(cmd, shell=True)
+    cmd = "srun -n 48 $BEAGLE_DIR/beagle-opt -i " + file_to_write
+    returned_value = subprocess.call(cmd, shell=True)
