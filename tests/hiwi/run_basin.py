0807870deaff7c2341e3dfbcaec508d80f97c1ac
Admin <geopwhuang@gmail.com>
2018-06-26 13:10:43 +0200

    Testing stage of the script.

--- tests/hiwi/run_basin.py
+++ tests/hiwi/run_basin.py
@@ -12,7 +12,7 @@ a2 = [0,0.125,0.25]
 a3 = [0,0.125,0.25]
 a4 = [0,0.125,0.25]
 
-x = '012'
+x = '0'
 label = []
 
 for i in product(x,x,x,x):
@@ -25,17 +25,31 @@ for c, i in enumerate(product(a1, a2, a3, a4)):
     string_to_write += str(i[3]) + '*sin(3.14*y)*cos(3.14*x/1.5)'
     string_to_write += "'\n"
 
-    f = open("voa_basin.i", "r")
+    f = open("~/projects/beagle/tests/hiwi/voa_basin.i", "r")
     contents = f.readlines()
     f.close()
 
     contents.insert(62, string_to_write)
 
-    file_to_write = "voa_basin_" + label[c] + '.i'
+    file_to_write = "~/projects/beagle/tests/hiwi/voa_basin_" + label[c] + '.i'
     f = open(file_to_write, "w")
     contents = "".join(contents)
     f.write(contents)
     f.close()
 
-    cmd = "srun -n 48 $BEAGLE_DIR/beagle-opt -i " + file_to_write
+    f = open("/homea/jhpc52/jhpc5202/job/beck_gen_base.j", "r")
+    contents = f.readlines()
+    f.close()
+
+    contents.insert(14, 'cd ~/projects/beagle/tests/hiwi\n')
+    contents.insert(15, 'srun -n 48 $BEAGLE_DIR/beagle-opt -i' + file_to_write)
+
+    file_to_write = "/homea/jhpc52/jhpc5202/job/beck_gen.j"
+    f = open(file_to_write, "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
+    cmd = 'sbatch /homea/jhpc52/jhpc5202/job/beck_gen.j'
     returned_value = subprocess.call(cmd, shell=True)
+    time.sleep(0.5)
