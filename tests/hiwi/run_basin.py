e8795bc7f89b38260f57ca2f115e7bfe6de32d50
Admin <geopwhuang@gmail.com>
2018-06-26 13:22:32 +0200

    Fixed a bug.

--- tests/hiwi/run_basin.py
+++ tests/hiwi/run_basin.py
@@ -3,9 +3,7 @@ import pandas as pd
 import subprocess
 import sys
 
-sys_arg = np.array(sys.argv) #the seed for the cubes to compute
-
-seed = int(sys_arg[1])
+sys_arg = np.array(sys.argv)
 
 a1 = [0,0.125,0.25]
 a2 = [0,0.125,0.25]
@@ -19,6 +17,7 @@ for i in product(x,x,x,x):
     label.append(i[0]+i[1]+i[2]+i[3])
 
 for c, i in enumerate(product(a1, a2, a3, a4)):
+    #Write to the MOOSE input file
     string_to_write = "value = '" + str(i[0]) + '*sin(3.14*y)*cos(3.14*z) + '
     string_to_write += str(i[1]) + '*sin(3.14*y)*cos(3.14*x/1.5)*cos(3.14*z) + '
     string_to_write += str(i[2]) + '*sin(3.14*y)*cos(2*3.14*x/1.5) + '
@@ -37,6 +36,7 @@ for c, i in enumerate(product(a1, a2, a3, a4)):
     f.write(contents)
     f.close()
 
+    #Write to the batch file
     f = open("/homea/jhpc52/jhpc5202/job/beck_gen_base.j", "r")
     contents = f.readlines()
     f.close()
